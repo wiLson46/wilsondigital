5fb55a362fce2e3225c1be9bd5ae2a05eff1332b
wilson <wilsondigital.online@gmail.com>
2023-04-28 18:12:44 -0300

    updates videos

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
+console.log("loaded");
+
 var scene = document.getElementById('animado');
 var parallaxInstance = new Parallax(animado);
+
+document.getElementById('back1').addEventListener('ended', myHandler, false);
+function myHandler(e) {
+    console.log('ended');
+    setTimeout(function () {
+        document.getElementById('back1').play();
+    }, 5000);
+    
+}
