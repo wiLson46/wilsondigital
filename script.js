3f3ace65d30544b553ad24802b59133164eb4945
wilson <wilsondigital.online@gmail.com>
2023-04-28 18:30:34 -0300

    +

--- script.js
+++ script.js
@@ -3,11 +3,26 @@ console.log("loaded");
 var scene = document.getElementById('animado');
 var parallaxInstance = new Parallax(animado);
 
-document.getElementById('back1').addEventListener('ended', myHandler, false);
-function myHandler(e) {
+document.getElementById('back1').addEventListener('ended', myHandler1, false);
+function myHandler1(e) {
     console.log('ended');
     setTimeout(function () {
         document.getElementById('back1').play();
-    }, 5000);
-    
+    }, 4000); 
+}
+
+document.getElementById('back2').addEventListener('ended', myHandler2, false);
+function myHandler2(e) {
+    console.log('ended');
+    setTimeout(function () {
+        document.getElementById('back2').play();
+    }, 3000); 
+}
+
+document.getElementById('top1').addEventListener('ended', myHandler3, false);
+function myHandler3(e) {
+    console.log('ended');
+    setTimeout(function () {
+        document.getElementById('top1').play();
+    }, 2000); 
 }
