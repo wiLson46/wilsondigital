feee6878cefb6fec2e390fc3cb206413f6503693
wilson <wilsondigital.online@gmail.com>
2023-05-04 01:11:35 -0300

    +

--- script.js
+++ script.js
@@ -8,15 +8,7 @@ function myHandler1(e) {
     console.log('ended');
     setTimeout(function () {
         document.getElementById('back1').play();
-    }, 4000); 
-}
-
-document.getElementById('back2').addEventListener('ended', myHandler2, false);
-function myHandler2(e) {
-    console.log('ended');
-    setTimeout(function () {
-        document.getElementById('back2').play();
-    }, 3000); 
+    }, 2000); 
 }
 
 document.getElementById('top1').addEventListener('ended', myHandler3, false);
